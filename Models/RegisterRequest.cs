864b113e96c7706286247b88605428a4bcd65ae2
roaaayman <119889441+roaaayman@users.noreply.github.com>
2025-04-29 23:04:13 +0300

    ..

---
+++ Models/RegisterRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace healthmate_backend.Models
+{
+    public class RegisterRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        public string Email { get; set; }
+        public bool AcceptedTerms { get; set; }
+    }
+}
