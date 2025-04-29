0af170fe7defb4b269512330b31b9bde542ea5b2
Rawan Tarek <142927339+rawanntarek@users.noreply.github.com>
2025-04-30 01:42:35 +0300

    patient complete profile

---
+++ Models/Request/PatientCompleteProfileRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace healthmate_backend.Models
+{
+    public class PatientCompleteProfileRequest
+    {
+        public float Height { get; set; }
+        public float Weight { get; set; }
+        public string BloodType { get; set; }
+        public DateTime Birthdate { get; set; }
+    }
+}
--- Services/PatientService.cs
+++ Services/PatientService.cs
@@ -1,1 +1,29 @@
-﻿
+﻿using healthmate_backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace healthmate_backend.Services
+{
+    public class PatientService
+    {
+        private readonly AppDbContext _context;
+
+        public PatientService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> CompleteProfileAsync(int id, PatientCompleteProfileRequest request)
+        {
+            var patient = await _context.Patients.FindAsync(id);
+            if (patient == null) return false;
+
+            patient.Height = request.Height;
+            patient.Weight = request.Weight;
+            patient.BloodType = request.BloodType;
+            patient.Birthdate = request.Birthdate;
+
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
