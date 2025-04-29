027f8976aace469a97f59bec4ffa2932f7d2a1f5
Rawan Tarek <142927339+rawanntarek@users.noreply.github.com>
2025-04-29 22:09:15 +0300

    jwt token

--- Program.cs
+++ Program.cs
@@ -1,20 +1,60 @@
 using healthmate_backend.Models;
+using healthmate_backend.Services;
 using Microsoft.EntityFrameworkCore;
-using Pomelo.EntityFrameworkCore.MySql.Infrastructure; // <-- Important
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Your MySQL connection string
 var connectionString = "server=mysql-2dbc541a-healthmate.k.aivencloud.com;port=15855;database=defaultdb;user=avnadmin;password=AVNS_lhFXyGW3wGvurVtSCVi;SslMode=Required;";
 
-// Add services to the container
+// Add DbContext
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseMySql(
         connectionString,
-        new MySqlServerVersion(new Version(8, 0, 35)) // <-- your MySQL server version
+        new MySqlServerVersion(new Version(8, 0, 35))
     )
 );
 
+// Add AuthenticationService
+builder.Services.AddScoped<AuthenticationService>();
+
+// Add IHttpContextAccessor
+builder.Services.AddHttpContextAccessor();
+
+// JWT Authentication
+var jwtKey = builder.Configuration["Jwt:Key"]; 
+var keyBytes = Encoding.UTF8.GetBytes(jwtKey);
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(keyBytes),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+
+// Add Controllers
+builder.Services.AddControllers();
+
 var app = builder.Build();
+
+// Middleware
+app.UseHttpsRedirection();
+app.UseAuthentication();  // 🔥 Add before Authorization
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
