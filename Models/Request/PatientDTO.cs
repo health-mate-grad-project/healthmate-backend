cb547d1113cd2c1caa05c795f5c6141a80eb5e0a
Itsyehia <125609890+Itsyehia@users.noreply.github.com>
2025-04-30 21:45:31 +0300

    add DTO for User and Patient

---
+++ Models/Request/PatientDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace healthmate_backend.Models.Request;
+
+public class PatientDTO
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public string BloodType { get; set; }
+    public float Height { get; set; }
+    public float Weight { get; set; }
+    public string Location { get; set; }
+}
---
+++ Models/Request/UserDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace healthmate_backend.Models.Request;
+
+public class UserDTO
+{
+    public string Username { get; set; }
+    public string Password { get; set; }  
+}
