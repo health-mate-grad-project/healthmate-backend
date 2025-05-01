9d82b830a48677c2a62a362949208c4874b01477
Rawan Tarek <142927339+rawanntarek@users.noreply.github.com>
2025-05-02 00:46:13 +0300

    doctor details

---
+++ Models/DTOs/DoctorDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace healthmate_backend.Models
+{
+    public class DoctorDto
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string License { get; set; }
+        public string Speciality { get; set; }
+        public int ExperienceYear { get; set; }
+        public double AverageRating { get; set; }
+        public int TotalRatings { get; set; }
+    }
+
+}
