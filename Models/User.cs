2245e4728a562ddeb5bec8c2e755996c0f1f0be1
Rawan Tarek <142927339+rawanntarek@users.noreply.github.com>
2025-05-02 00:55:58 +0300

    db

--- Models/User.cs
+++ Models/User.cs
@@ -7,5 +7,6 @@
         public required string Password { get; set; }
         public required string Email { get; set; }
         public required string Type { get; set; }
+		public string? ProfileImageUrl { get; set; }
     }
 }
