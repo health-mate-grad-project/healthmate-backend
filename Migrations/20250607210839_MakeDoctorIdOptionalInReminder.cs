73429428ae2f2ec09d751d4bb53093a69f0d3b62
Rawan Tarek <142927339+rawanntarek@users.noreply.github.com>
2025-06-08 00:24:30 +0300

    .

---
+++ Migrations/20250607210839_MakeDoctorIdOptionalInReminder.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace healthmate_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeDoctorIdOptionalInReminder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndTime",
+                table: "AvailableSlots");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DayOfWeek",
+                table: "AvailableSlots",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DayOfWeek",
+                table: "AvailableSlots");
+
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "EndTime",
+                table: "AvailableSlots",
+                type: "time(6)",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0));
+        }
+    }
+}
