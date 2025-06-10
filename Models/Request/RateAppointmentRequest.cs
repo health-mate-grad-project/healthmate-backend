46475a41e78b811f300abc4a1fa91ed2c9da20fc
Rawan Tarek <142927339+rawanntarek@users.noreply.github.com>
2025-06-10 18:41:17 +0300

    rate

---
+++ Models/Request/RateAppointmentRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace healthmate_backend.Models.Request;
+
+public class RateAppointmentRequest
+{
+    public required int AppointmentId { get; set; }
+    public required int Rating { get; set; }
+}
