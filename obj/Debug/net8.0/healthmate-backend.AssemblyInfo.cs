26fc72e3d35b3e96e93d6f5ea1d7f464dae143f7
Rawan Tarek <142927339+rawanntarek@users.noreply.github.com>
2025-06-23 17:22:30 +0300

    .

--- obj/Debug/net8.0/healthmate-backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/healthmate-backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("healthmate-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36ec9bfd89f8841da0f90c29de7e95660590ef90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbff027831ca7a37434d51e74e661dd8ffaf1ad3")]
 [assembly: System.Reflection.AssemblyProductAttribute("healthmate-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("healthmate-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
